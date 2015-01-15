5da83ae0cdf61f32154d2978d87133b72ed7c8fc
Sajjad Zaheer <szaheer.tech@gmail.com>
2015-01-15 19:08:08 +0500

    initial code of module

---
+++ README.txt
@@ -0,0 +1,1 @@
+Custom form field comprising chain of dependent select fields that can be plugged into Drupal forms programmatically.
---
+++ ajax_chain_select.js
@@ -0,0 +1,47 @@
+function f3region_get_element(el, $previous_level) {
+  var previous_level_id = el.value,
+          url = Drupal.settings.basePath + 'ajax_chain_select/callback/',
+          $formitem = jQuery(el).closest('.form-item'),
+          $nextall = {},
+          $nextel = null,
+          $dc = null,
+          $next_level = $previous_level + 1;
+
+  $nextall = $formitem.nextAll();
+  $nextel = jQuery($nextall[0]);
+
+  for (var $i = 1; $i < $nextall.length; $i++) {
+    var $next_ptr = jQuery($nextall[$i]);
+    if ($next_ptr.hasClass('form-type-textfield')) {
+      continue;
+    } else {
+      $next_ptr.addClass('form-disabled').find('select').attr('disabled', 'disabled');
+      if ($next_ptr.find('select').find('option') != 'undefined') {
+        $next_ptr.find('select').find('option:eq(0)').attr('selected', 'selected');
+      }
+    }
+  }
+
+  $dc = jQuery(el).closest('fieldset').find('.form-type-textfield input').val();
+
+  if (previous_level_id == "" || isNaN(Number(previous_level_id))) {
+    $nextel.addClass('form-disabled').find('select').attr('disabled', 'disabled');
+    if ($nextel.find('select').find('option') != 'undefined') {
+      $nextel.find('select').find('option:eq(0)').attr('selected', 'selected');
+    }
+  } else {
+    jQuery.getJSON(url + $next_level + '/' + previous_level_id + '/' + $dc, function(response) {
+      if (response.has_data == '1') {
+        var $temp = $nextel.find('select').find('option:eq(0)');
+        $nextel.find('select').html($temp);
+        $nextel.find('select').append(response.options);
+        $nextel.removeClass('form-disabled').find('select').removeAttr('disabled');
+      } else {
+        $nextel.addClass('form-disabled').find('select').attr('disabled', 'disabled');
+        if ($nextel.find('select').find('option') != 'undefined') {
+          $nextel.find('select').find('option:eq(0)').attr('selected', 'selected');
+        }
+      }
+    });
+  }
+}
